33c295cfa259dcd0b0bec1ed589fa01c5510a202
Iwanaga-Sena <171421410+Iwanaga-Sena@users.noreply.github.com>
2024-07-07 12:50:44 +0900

    [add] Books

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_one_attached :image
   has_many :books, dependent: :destroy
 
-  def get_image
+  def get_profile_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
